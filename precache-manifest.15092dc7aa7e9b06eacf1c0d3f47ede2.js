e03a26f18a012ea0e5ea4387818bfad6c083691c
omkar Kesarkhane <omkesarkhane@gmail.com>
2020-06-17 12:07:17 +0530

    Updates

---
+++ precache-manifest.15092dc7aa7e9b06eacf1c0d3f47ede2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8ac4aed2b431f250147a785681616e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bb01a4890b247eeff28",
+    "url": "/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "2e9b66b08c7347b15e33",
+    "url": "/static/css/main.ac4a0987.chunk.css"
+  },
+  {
+    "revision": "8bb01a4890b247eeff28",
+    "url": "/static/js/2.27dc0fd0.chunk.js"
+  },
+  {
+    "revision": "5a679c6850c87830997be51c063e1968",
+    "url": "/static/js/2.27dc0fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9b66b08c7347b15e33",
+    "url": "/static/js/main.e7808144.chunk.js"
+  },
+  {
+    "revision": "e9faf038bb1020780332",
+    "url": "/static/js/runtime-main.2069ec65.js"
+  }
+]);
